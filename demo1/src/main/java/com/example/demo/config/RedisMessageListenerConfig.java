dc099a90bcc9eb17b7203980c801a3ebf98608e6
Sorin Turda <s.turda12@gmail.com>
2025-04-14 00:00:36 +0300

    subscriber adaugat

---
+++ demo1/src/main/java/com/example/demo/config/RedisMessageListenerConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+import com.example.demo.subscriber.MessageSubscriber;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisMessageListenerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, MessageListener messageListener) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListener, new PatternTopic("your-channel"));
+        return container;
+    }
+
+    @Bean
+    MessageListener messageListener() {
+        return new MessageSubscriber();
+    }
+}
